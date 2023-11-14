2f1212852d8726b29041d37f1d6b8a4fc5b64600
Aaron Blenkush <aaronblenkush@gmail.com>
2023-11-14 00:37:16 -0900

    app change loading

---
+++ src/lib/api.js
@@ -0,0 +1,29 @@
+import { browser } from "$app/environment";
+
+
+
+export function api(customFetch = fetch) {
+    return {
+        getDictionary: async () => {
+            let res = await customFetch("./scrabble-dictionary.txt");
+            let txt = await res.text();
+
+            let dictionary = txt.split("\n")
+                .map(line => {
+                    return line.trim().split(" ")[1];
+                })
+            ;
+            let solver = dictionary.reduce(
+                ((acc,word) => {
+                    let token = [...word].sort().join('');
+                    acc[token] = acc[token] || [];
+                    acc[token].push(word);
+                    return acc;
+                }),
+                {}
+            );
+            console.log('done');
+            return { dictionary, solver };
+        },
+    };
+}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+import Unscrabbled from '$lib/Unscrabbled.svelte';
+import { onMount } from 'svelte';
+import { api } from '$lib/api';
+
+let data;
+
+onMount(async () => {
+    data = {
+        ...await api(fetch).getDictionary(),
+        disabled: false,
+    }
+});
+
+</script>
+<Unscrabbled {data} />
